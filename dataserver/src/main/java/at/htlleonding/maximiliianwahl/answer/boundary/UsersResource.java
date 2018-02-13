381ca050b1547d023cc0197872bff3468bca6541
Maximilian Wahl <mexx.wale@gmail.com>
2018-02-13 11:05:00 +0100

    began implementation of backend
    began implementation of backend with some simple testdata.

---
+++ dataserver/src/main/java/at/htlleonding/maximiliianwahl/answer/boundary/UsersResource.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.htlleonding.maximiliianwahl.answer.boundary;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author mexxw
+ */
+@Path("users")
+public class UsersResource {
+    
+    @GET
+    public JsonObject answer(){
+        return Json.createObjectBuilder()
+                .add("Firstname", "Max")
+                .add("Lastname","Mustermann")
+                .add("Email", "max.mustermann@gmx.com")
+                .add("Birthdate", "1.1.1111")
+                .add("Username","admin")
+                .add("password", "admin")
+                .build();
+    }
+}
