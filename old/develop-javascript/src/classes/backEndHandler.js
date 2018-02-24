8d13ad08330d9a2e35a038bd2d43f75f55413d6d
Unknown <konstantinurbanides@gmx.at>
2018-02-24 19:14:56 +0100

    unit pages created

--- old/develop-javascript/src/classes/backEndHandler.js
+++ old/develop-javascript/src/classes/backEndHandler.js
@@ -1,6 +1,7 @@
 import User from "./user.js";
 import Theme from "./theme.js";
-
+import Unit from "./unit.js";
+import Word from "./word.js";
 
 export default class BackEndHandler{
 
@@ -15,7 +16,7 @@ export default class BackEndHandler{
     }
 
     static async login(username,password){
-        const {id,Firstname,Lastname,Email,Birthdate,Username,Password} = await this.answer("http://localhost:8080/dataserver/webresources/users/login?user="+username+"&pw="+password);
+        const {id,Firstname,Lastname,Email,Birthdate,Username,Password,Institution} = await this.answer("http://localhost:8080/dataserver/webresources/users/login?user="+username+"&pw="+password);
         if(id ==-1){
             return null;
         }
@@ -25,11 +26,12 @@ export default class BackEndHandler{
         user.setLastname(Lastname);
         user.setEmail(Email);
         user.setBirthdate(Birthdate);
+        user.setInstitution(Institution);
         return user;
     }
 
     static async user(username){
-        const {id,Firstname,Lastname,Email,Birthdate,Username,Password} = await this.answer("http://localhost:8080/dataserver/webresources/users/user?user="+username);
+        const {id,Firstname,Lastname,Email,Birthdate,Username,Password,Institution} = await this.answer("http://localhost:8080/dataserver/webresources/users/user?user="+username);
         if(id ==-1){
             return null;
         }
@@ -39,22 +41,62 @@ export default class BackEndHandler{
         user.setLastname(Lastname);
         user.setEmail(Email);
         user.setBirthdate(Birthdate);
+        user.setInstitution(Institution);
         return user;
     }
     static async startingTheme(uId){
-        const {name,headerBackgroundColor,headerFontColor,menuBackgroundColor,menuFontColor,menuNavigationFontColor,cardAreaBackgroundColor} = await this.answer("http://localhost:8080/dataserver/webresources/themes/startingTheme/"+uId);
-        theme = new Theme(name,headerBackgroundColor,menuFontColor,headerFontColor,cardAreaBackgroundColor,menuNavigationColor,menuBackgroundColor,menuNavigationColor);
+        const {id,name,headerBackgroundColor,headerFontColor,menuBackgroundColor,menuFontColor,menuNavigationFontColor,cardAreaBackgroundColor,menuNavigationColor} = await this.answer("http://localhost:8080/dataserver/webresources/themes/startingTheme/"+uId);
+        theme = new Theme(id,name,headerBackgroundColor,menuFontColor,headerFontColor,cardAreaBackgroundColor,menuNavigationColor,menuBackgroundColor,menuNavigationFontColor);
         return theme;      
     }
     static async userThemes(uId){
         let jsonText = await this.answer("http://localhost:8080/dataserver/webresources/themes/userThemes/"+uId);
         let themes = [];
-        var newArr = JSON.parse(jsonText);
-        while(newArr.length>0){
-            const {name,headerBackgroundColor,headerFontColor,menuBackgroundColor,menuFontColor,menuNavigationFontColor,cardAreaBackgroundColor} = newArr.pop();
-            theme = new Theme(name,headerBackgroundColor,menuFontColor,headerFontColor,cardAreaBackgroundColor,menuNavigationColor,menuBackgroundColor,menuNavigationColor);
+        while(jsonText.length>0){
+            const {id,name,headerBackgroundColor,headerFontColor,menuBackgroundColor,menuFontColor,menuNavigationFontColor,cardAreaBackgroundColor,menuNavigationColor} = jsonText.pop();
+            let theme = new Theme(id,name,headerBackgroundColor,menuFontColor,headerFontColor,cardAreaBackgroundColor,menuNavigationColor,menuBackgroundColor,menuNavigationFontColor);
             themes.push(theme);
         }
         return themes;
+    }
+    static async insertTheme(uID,theme){
+        const {retVal} = await this.answer("localhost:8080/dataserver/webresources/themes/newTheme?owner="+uID+"&name="+theme.getName()+"&hBG="+theme.getHeaderBackgroundColor()+"&mFC="+theme.getMenuFontColor()+"&hFC="+theme.getHeaderFontColor()+"&cABG="+theme.getCardAreaBackgroundColor()+"&mNC="+theme.getMenuNavigationColor()+"&mBG="+theme.getMenuBackgroundColor()+"&mNF="+theme.getMenuNavigationFontColor());
+        return retVal;
+    }
+    static async changeTheme(theme){
+        const {retVal} = await this.answer("localhost:8080/dataserver/webresources/themes/changeTheme?theme="+theme.getId()+"&name="+theme.getName()+"&hBG="+theme.getHeaderBackgroundColor()+"&mFC="+theme.getMenuFontColor()+"&hFC="+theme.getHeaderFontColor()+"&cABG="+theme.getCardAreaBackgroundColor()+"&mNC="+theme.getMenuNavigationColor()+"&mBG="+theme.getMenuBackgroundColor()+"&mNF="+theme.getMenuNavigationFontColor());
+        return retVal;
+    }
+    static async changeStartingTheme(uId,tId){
+        const {retVal} = await this.answer("localhost:8080/dataserver/webresources/users/setStartingTheme?user="+uId+"&theme="+tId);
+        return retVal;
+    }
+    static async changeUser(user){
+        const {retVal} = await this.answer("localhost:8080/dataserver/webresources/users/changeUser?user="+user.getId()+"&user="+user.getUsername()+"&pw="+user.getPassword()+"&fN="+user.getFirstName()+"&lN="+user.getLastname()+"&Email="+user.getEmail()+"&bD="+user.getBirthdate()+"&inst="+user.getInstitution());
+        return retVal;
+    }
+    static async deleteTheme(tId){
+        const {retVal} = await this.answer("localhost:8080/dataserver/webresources/themes/deleteTheme?theme="+tId);
+        return retVal;
+    }
+    static async getUnits(){
+        let jsonText = await this.answer("http://localhost:8080/dataserver/webresources/units/units");
+        let units = [];
+        while(jsonText.length>0){
+            const {name} = jsonText.pop();
+            let unit = new Unit(name);
+            units.push(unit);
+        }
+        return units;
+    }
+    static async getWords(unitName){
+        let jsonText = await this.answer("http://localhost:8080/dataserver/webresources/units/words/"+unId);
+        let words = [];
+        while(jsonText.length>0){
+            const {wordGerman,wordEnglisch} = jsonText.pop();
+            let word = new Word(wordGerman,wordEnglisch);
+            words.push(word);
+        }
+        return words;
     }
 }
